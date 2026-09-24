9ec0a5175c5c9871eb7f591240c117213c3c660f
JoFi1977 <20jofi00@gmail.com>
2026-09-24 15:10:11 +0300

    realize tests for func(is_isogram)

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,19 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    ("word", "expected"),
+    [
+        pytest.param("", True, id="empty string"),
+        pytest.param("a", True, id="single letter"),
+        pytest.param("equal", True, id="unique letters"),
+        pytest.param("apply", False, id="consecutive duplicate"),
+        pytest.param("WORLD", True, id="unique uppercase letters"),
+        pytest.param("underground", False, id="non-consecutive duplicates"),
+        pytest.param("PArameTRIzE", False, id="different case duplicate"),
+    ],
+)
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert is_isogram(word=word) is expected
